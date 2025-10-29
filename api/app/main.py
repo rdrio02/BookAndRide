174d28d1842f2fe36fc0263fb1c6949c1bfb434b
rdrio02 <rodrigo.laureano090@gmail.com>
2025-10-29 20:44:57 +0100

    Release v1.0.1

--- api/app/main.py
+++ api/app/main.py
@@ -264,6 +264,7 @@ async def stop_rental(request: Request, db: Session = Depends(get_db), user=Depe
     data = parse_body(body, content_type, RENTAL_STOP_SCHEMA, schemas.RentalStopIn)
 
     print("Received rental start data:", json.dumps(data, indent=2))
+    print("This print is a feature of version 1.0.1")
 
     r = db.get(models.Rental, data["rental_id"])
     if not r:
