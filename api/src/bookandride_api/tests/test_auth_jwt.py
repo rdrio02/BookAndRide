7f8cf0acd2debbf4be78744d1291789ca4aa322f
rdrio02 <rodrigo.laureano090@gmail.com>
2025-12-03 18:47:49 +0100

    change to test

--- api/src/bookandride_api/tests/test_auth_jwt.py
+++ api/src/bookandride_api/tests/test_auth_jwt.py
@@ -1,17 +1,20 @@
+import pytest
 from fastapi.testclient import TestClient
 from bookandride_api.main import app
 
-client = TestClient(app)
-def test_register_login_and_access_books():
-    
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+def test_register_login_and_access_books(client):  # <--- note 'client' argument
     # register
     r = client.post("/register",
-    json={"email":"bob@example.com","password":"Secret123"})
+                    json={"email":"bob@example.com","password":"Secret123"})
     assert r.status_code in (200, 201)
-    
+
     # login
     r = client.post("/login",
-    json={"email":"bob@example.com","password":"Secret123"})
+                    json={"email":"bob@example.com","password":"Secret123"})
     assert r.status_code == 200
     token = r.json()["access_token"]
     assert token
